d8f0b57913399687f0f19b77319c2ccac63a1b32
siliconx <siliconx@163.com>
2019-12-14 16:51:49 +0800

    modify config

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 JW_USERNAME = '19Sxxx'  # 教务系统账号--学号
-JW_PASSWORD = 'xxxpass'  # 教务系统密码
+JW_PASSWORD = 'xxx'  # 教务系统密码
 
 EMAIL_HOST = 'smtp.qq.com'  # smtp服务器
 EMAIL_USERNAME = 'xxx@qq.com'  # 发送方邮箱
-EMAIL_PASSWORD = 'abcd'  # 客户端授权码，非普通登录密码
+EMAIL_PASSWORD = 'xxx'  # 客户端授权码，非普通登录密码
 EMAIL_RECEIVERS = ['xxx@163.com']  # 接收方邮箱，可有多个
+
